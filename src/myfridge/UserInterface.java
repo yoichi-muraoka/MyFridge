8cbc295b08351cf6b5d49ed00389327ba5df4011
zdis56 <zdis56@sim.zdrv.com>
2021-09-27 17:14:24 +0900

    システムの終了を選択するまでループさせる

--- src/myfridge/UserInterface.java
+++ src/myfridge/UserInterface.java
@@ -11,25 +11,27 @@ public class UserInterface {
 	 * ユーザーの選択した番号に応じて、各画面を呼び出す
 	 */
 	public void run() {
-		// ユーザーに操作を選択させる
-		System.out.println("何をしますか？");
-		System.out.println("1:冷蔵庫の中身を見る  2:賞味期限切れ一覧  3:追加  4:削除  5:終了");
+		boolean loopFlag = true;
+		while(loopFlag) {
+			// ユーザーに操作を選択させる
+			System.out.println("何をしますか？");
+			System.out.println("1:冷蔵庫の中身を見る  2:賞味期限切れ一覧  3:追加  4:削除  5:終了");
 
-		var scanner = new Scanner(System.in);
-		String strChoice = scanner.nextLine();
+			var scanner = new Scanner(System.in);
+			String strChoice = scanner.nextLine();
 
-		// 入力された番号を整数に変換
-		int choice;
-		try {
-			choice = Integer.parseInt(strChoice);
-		}
-		catch(NumberFormatException e) {
-			// 変換できなかった場合、「5:終了」とする
-			choice = 5;
-		}
+			// 入力された番号を整数に変換
+			int choice;
+			try {
+				choice = Integer.parseInt(strChoice);
+			}
+			catch(NumberFormatException e) {
+				// 変換できなかった場合、「5:終了」とする
+				choice = 5;
+			}
 
-		// 番号に応じて画面を呼び出す
-		switch(choice) {
+			// 番号に応じて画面を呼び出す
+			switch(choice) {
 			case 1:
 				showItems();
 				break;
@@ -43,10 +45,11 @@ public class UserInterface {
 				deleteItem();
 				break;
 			default:
-				System.out.println("終了");
+				System.out.println("システムを終了しました");
+				scanner.close();
+				loopFlag = false;
+			}
 		}
-
-		scanner.close();
 	}
 
 
