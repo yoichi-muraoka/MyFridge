7e6f02df990c4d790636ec7a7697d38341462ba6
zdis56 <zdis56@sim.zdrv.com>
2021-09-28 12:16:16 +0900

    賞味期限切れ判定のメソッドを追加

--- src/myfridge/ItemServiceImpl.java
+++ src/myfridge/ItemServiceImpl.java
@@ -1,6 +1,9 @@
 package myfridge;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -10,9 +13,21 @@ import java.util.List;
 public class ItemServiceImpl implements ItemService {
 
 	private ListTextConverter converter;
+	private Date today; // 今日の日付(賞味期限切れ確認用)
 
 	public ItemServiceImpl() {
 		converter = new ListTextConverter();
+
+		// 現在日時から時間を切り捨てて「今日の日付」として設定
+		// 参考：https://teratail.com/questions/1123
+		var sdf = new SimpleDateFormat("yyyy/MM/dd");
+		String strToday = sdf.format(new Date());
+		try {
+			today = sdf.parse(strToday);
+		}
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
 
@@ -54,6 +69,18 @@ public class ItemServiceImpl implements ItemService {
 	public void deleteItem(int index) {
 		// TODO 自動生成されたメソッド・スタブ
 
+	}
+
+
+	/**
+	 * 参考：https://www.sejuku.net/blog/20714
+	 *
+	 * 賞味期限切れか否か判断する(賞味期限と現在の日付を比較)
+	 * @param expDate 賞味期限
+	 * @return 期限切れの場合、trueを返す
+	 */
+	private boolean isExpired(Date expDate) {
+		return expDate.compareTo(today) == -1;
 	}
 
 }
