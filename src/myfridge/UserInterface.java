28c772c74ef6acbc62b4c14a02f53e86ca5780be
zdis56 <zdis56@sim.zdrv.com>
2021-09-28 13:42:55 +0900

    削除操作選択時にアイテム一覧を表示

--- src/myfridge/UserInterface.java
+++ src/myfridge/UserInterface.java
@@ -140,7 +140,10 @@ public class UserInterface {
 	 */
 	private void deleteItem() {
 		System.out.println("何番のアイテムを削除しますか？");
-		System.out.println("キャンセルの場合、適当なアルファベットを入力してください");
+		System.out.println("キャンセルの場合、適当なアルファベットを入力してください\n");
+
+		showItems();
+
 		String strIndex = scanner.nextLine();
 
 		try {
